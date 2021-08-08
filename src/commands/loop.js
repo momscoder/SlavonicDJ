378d38dc0a917808d28f6d2db6ae328391905928
momscoder <dimagfenics@gmail.com>
2021-08-08 20:29:10 +0300

    loop command

---
+++ src/commands/loop.js
@@ -0,0 +1,29 @@
+module.exports = {
+  name: "loop",
+  aliases: ["l", "repeat"],
+  description: "Music command",
+  async execute(message) {
+    const player = message.client.manager.get(message.guild.id);
+    if (!player) return message.reply("Сейчас ничего не играет");
+
+    const { channel } = message.member.voice;
+
+    if (!channel)
+      return message.reply(
+        "Вы должны находиться в голосовом канале для использования этой команды!"
+      );
+    if (channel.id !== player.voiceChannel)
+      return message.reply("Вы находитесь не в том голосовом канале...");
+
+    if (!player.queue.length)
+      return message.channel.send("```0_0 Очередь пуста```");
+
+    if (player.queueRepeat) {
+      player.setQueueRepeat(false);
+      return message.channel.send("Очередь больше не зацикливается!");
+    } else {
+      player.setQueueRepeat(true);
+      return message.channel.send("Зациклил очередь!");
+    }
+  },
+};
