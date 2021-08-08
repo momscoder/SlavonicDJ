4703db4f7cff3e6aed9e636359f90f0879cd0874
momscoder <dimagfenics@gmail.com>
2021-08-08 20:28:32 +0300

    queue update

--- src/commands/queue.js
+++ src/commands/queue.js
@@ -14,13 +14,21 @@ module.exports = {
 
     const tracks = queue.slice(0, 10);
 
-    if (queue.current)
+    /*if (queue.current)
       answer += `> ${queue.current.title} (-${Duration.fromMillis(
         queue.current.duration - player.position
-      ).toFormat("mm:ss")})\n`;
+      ).toFormat("mm:ss")})\n`;*/
 
     if (tracks.length)
-      answer += tracks.map((track, i) => `${++i}) ${track.title}`).join("\n");
+      answer += tracks
+        .map((track, i) =>
+          queue.pointer === i
+            ? `> ${++i}) ${track.title} (-${Duration.fromMillis(
+                queue.current.duration - player.position
+              ).toFormat("mm:ss")})`
+            : `${++i}) ${track.title}`
+        )
+        .join("\n");
 
     return message.channel.send(
       "```" + (answer.length ? answer : " 0_0 Очередь пуста") + "```"
