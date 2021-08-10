b5efc8a14601e66e6ba6bb92d195b6ed67c62c58
momscoder <dimagfenics@gmail.com>
2021-08-10 18:17:33 +0300

    skip can "skip n tracks"

--- src/commands/skip.js
+++ src/commands/skip.js
@@ -1,10 +1,10 @@
 module.exports = {
   name: "skip",
   aliases: ["s"],
-  description: "Пропускает один трек",
-  async execute(message) {
+  description: "Пропускает n треков (по умолчанию 1)",
+  async execute(message, args) {
     const player = message.client.manager.get(message.guild.id);
-    if (!player) return message.reply("Cейчас ничего не играет");
+    if (!player?.queue.current) return message.reply("Cейчас ничего не играет");
 
     const { channel } = message.member.voice;
 
@@ -16,11 +16,14 @@ module.exports = {
     if (channel.id !== player.voiceChannel)
       return message.reply("Вы находитесь не в том голосовом канале...");
 
-    if (!player.queue.current) return message.channel.send("Нечего пропускать");
-
     const { title } = player.queue.current;
 
     if (!player.playing) player.playing = true;
+    if (args.length && Number.isInteger(+args[0]) && +args[0] !== 1) {
+      player.queue.pointer += +args[0] - 1;
+      player.stop();
+      return message.channel.send(`Пропущено **${args[0]}** песен`);
+    }
     player.stop();
     return message.channel.send(`Песня **${title}** пропущена.`);
   },
